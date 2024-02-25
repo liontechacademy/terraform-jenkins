7623597dc09878ae6b7dfc153032792577c9bcda
prof Prince <info@liontechsolutions.com>
2024-02-25 22:40:23 +0000

    jenkins -infra

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "class21-devops"
-    key = "jenkins/terraform.tfstate"
+    key = "jenkins"
     region = "ca-central-1"
    # dynamodb_table = "my-dynamo-db-table-tf"
   }
